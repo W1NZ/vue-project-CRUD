baa101148fe36298effaeedd0ebbc4bd347f22e1
Mariusz Czapiewski <m4riusz.cz@gmail.com>
2023-11-13 18:28:52 +0100

    Created list elements

--- app.vue
+++ app.vue
@@ -2,6 +2,17 @@
   <header>Biuro Obsługi Studenta</header>
   <main>
     <h1>WNIOSKI</h1>
-    <button class="button">Button</button>
+    <div class="list">
+      <div class="list-element">
+        <span>
+          <div class="title">Wniosek o stypendium</div>
+          <div class="info">Jan Kowalski 654343</div>
+        </span>
+        <div class="buttons">
+          <button>Edit</button>
+          <button>Delete</button>
+        </div>
+      </div>
+    </div>
   </main>
 </template>
