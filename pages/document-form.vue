333e4c8c6319231ad8b72beee888736cb1be087b
Mariusz Czapiewski <m4riusz.cz@gmail.com>
2023-11-27 23:24:58 +0100

    Created create, delete, and read functionality

--- pages/document-form.vue
+++ pages/document-form.vue
@@ -2,13 +2,13 @@
   <header><NuxtLink to="/">Biuro Obsługi Studenta</NuxtLink></header>
   <main>
     <h1>TRYB EDYCJI</h1>
-    <form @submit.prevent="submitForm" class="formularz">
+    <form @submit.prevent="storeData" class="formularz">
       <div>
         <input
           class="text-input"
           type="text"
           id="title"
-          v-model="formData.title"
+          v-model="title"
           required
           placeholder="TYTUŁ"
         />
@@ -18,7 +18,7 @@
           class="text-input"
           type="text"
           id="name"
-          v-model="formData.name"
+          v-model="name"
           required
           placeholder="IMIĘ"
         />
@@ -28,7 +28,7 @@
           class="text-input"
           type="text"
           id="surname"
-          v-model="formData.surname"
+          v-model="surname"
           required
           placeholder="NAZWISKO"
         />
@@ -37,8 +37,8 @@
         <input
           class="text-input"
           type="text"
-          id="index"
-          v-model="formData.index"
+          id="album"
+          v-model="album"
           required
           placeholder="NR ALBUMU"
         />
@@ -48,13 +48,13 @@
         <input type="file" id="file" @change="handleFileChange" />
       </div>
       <div class="buttons">
-        <NuxtLink to="/documents"
-          ><button type="submit"><Icon name="fa6-solid:floppy-disk" /></button
-        ></NuxtLink>
-        <NuxtLink to="/documents"
-          ><button type="button" @click="cancelForm">
-            <Icon name="fa6-solid:circle-xmark" /></button
-        ></NuxtLink>
+        <button type="submit" @click="submitForm">
+          <Icon name="fa6-solid:floppy-disk" />
+        </button>
+
+        <button type="button" @click="cancelForm">
+          <Icon name="fa6-solid:circle-xmark" />
+        </button>
       </div>
     </form>
   </main>
@@ -67,28 +67,37 @@ export default {
         title: "",
         name: "",
         surname: "",
-        index: "",
+        album: "",
         file: null,
       },
     };
   },
   methods: {
+    storeData() {
+      let users = JSON.parse(localStorage.getItem("users") || "[]");
+      users.push({
+        title: this.title,
+        name: this.name,
+        surname: this.surname,
+        album: this.album,
+      });
+      localStorage.setItem("users", JSON.stringify(users));
+    },
     submitForm() {
-      // Handle form submission logic
       console.log("Form submitted:", this.formData);
+      window.location.href = "/documents";
     },
     cancelForm() {
-      // Handle form cancellation logic
       console.log("Form canceled");
+      window.location.href = "/documents";
     },
     handleFileChange(event) {
-      // Handle file change logic
       this.formData.file = event.target.files[0];
     },
   },
 };
 </script>
-<style scoped>
+<style scoped lang="scss">
 .formularz {
   display: flex;
   flex-direction: column;
@@ -106,6 +115,7 @@ export default {
   width: 100%;
   color: #bfdbf7;
   outline: none;
+  font-size: 25px;
 }
 .text-input::placeholder {
   color: #bfdbf7;
@@ -122,7 +132,7 @@ button {
   display: flex;
   padding: 5px;
   color: #bfdbf7;
-  font-size: 20px;
+  font-size: 35px;
 }
 input[type="file"] {
   display: none;
@@ -130,7 +140,7 @@ input[type="file"] {
 .custom-file-upload {
   background-color: #bfdbf7;
   text-align: left;
-  font-size: 16px;
+  font-size: 24px;
   border-radius: 10px;
   display: flex;
   padding: 10px 40px 10px;
--- pages/documents.vue
+++ pages/documents.vue
@@ -2,17 +2,19 @@
   <header><NuxtLink to="/">Biuro Obsługi Studenta</NuxtLink></header>
   <main>
     <h1>WNIOSKI</h1>
-    <div class="list">
+    <div class="list" v-for="(user, index) in users" :key="index">
       <div class="list-element">
         <span>
-          <div class="title">Wniosek o stypendium</div>
-          <div class="info">Jan Kowalski 654343</div>
+          <div class="title">Wniosek o {{ user.title }}</div>
+          <div class="info">
+            {{ user.name }} {{ user.surname }} {{ user.album }}
+          </div>
         </span>
         <div class="buttons">
           <NuxtLink to="/document-form"
             ><button title="Edytuj wniosek"><Icon name="fa-solid:pen" /></button
           ></NuxtLink>
-          <button title="Usuń wniosek">
+          <button @click="deleteUser(index)" title="Usuń wniosek">
             <Icon name="fa-solid:trash-alt" />
           </button>
         </div>
@@ -24,3 +26,23 @@
     ></NuxtLink>
   </main>
 </template>
+<script>
+export default {
+  data() {
+    return {
+      users: [],
+    };
+  },
+  mounted() {
+    if (process.client) {
+      this.users = JSON.parse(localStorage.getItem("users") || "[]");
+    }
+  },
+  methods: {
+    deleteUser(index) {
+      this.users.splice(index, 1);
+      localStorage.setItem("users", JSON.stringify(this.users));
+    },
+  },
+};
+</script>
